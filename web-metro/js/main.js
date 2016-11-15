42c40d66d79e7fff189330b9d92b7748c979b668
Quin Kennedy <quinkennedy@gmail.com>
2016-11-15 13:32:59 -0800

    linted web-metro

--- web-metro/js/main.js
+++ web-metro/js/main.js
@@ -1,14 +1,21 @@
+/* global Spacebrew */
+
 $(document).ready( function() {
-	setupsb();
+  setupsb();
 });
 
+/**
+ * Get User Param: gets values from the query string
+ * @param {string} name - the key to lookup in the query string
+ * @returns {string} the associated value in the query string
+ */
 function gup( name ) {
-  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  name = name.replace(/[\[]/,'\\\[').replace(/[\]]/,'\\\]');
+  var regexS = '[\\?&]'+name+'=([^&#]*)';
   var regex = new RegExp( regexS );
   var results = regex.exec( window.location.href );
   if( results == null )
-    return "";
+    return '';
   else
     return results[1];
 }
@@ -17,7 +24,11 @@ var delay = parseInt(gup('delay'));
 var currInterval;
 
 var sb;
-var setupsb = function(){
+
+/**
+ * Configure this Spacebrew Client and connect to the Spacebrew server
+ */
+function setupsb(){
   sb = new Spacebrew.Client(undefined,
                             'metronome ' + Math.floor(Math.random() * 1000),
                             'This outputs a boolean every so often.',
@@ -27,12 +38,12 @@ var setupsb = function(){
   sb.addSubscribe('delay', 'number');
   sb.addSubscribe('sync', 'boolean');
 
-  sb.onBooleanMessage = function onBoolean(name, value){
+  sb.onBooleanMessage = function onBoolean(/*name, value*/){
     setDelay(delay);
     sendBang();
   };
-  sb.onCustomMessage = function onCustom(name, value, type){
-    var v = parseInt(currValue);
+  sb.onCustomMessage = function onCustom(name, value/*, type*/){
+    var v = parseInt(value);
     if (v == v) {
       delay = v;
       setDelay(delay);
@@ -40,30 +51,38 @@ var setupsb = function(){
   };
 
   sb.onOpen = function onOpen(){
-    console.log("WebSockets connection opened");
+    console.log('WebSockets connection opened');
     if (delay == delay){
       setDelay(delay);
     } else {
       //NaN
-      document.getElementById("output").innerHTML = 
-        "Send a number message to set metro delay.";
+      document.getElementById('output').innerHTML = 
+        'Send a number message to set metro delay.';
     }
   };
   sb.onClose = function onClose(){
-    console.log("WebSockets connection closed");
-    document.getElementById("output").innerHTML = 
-      "no longer connected to spacebrew :(";
+    console.log('WebSockets connection closed');
+    document.getElementById('output').innerHTML = 
+      'no longer connected to spacebrew :(';
   };
   
   sb.connect();
-};
+}
 
+/**
+ * Set the metronome delay
+ * @param {number} d - delay in milliseconds between ticks
+ */
 function setDelay(d){
   clearInterval(currInterval);
   currInterval = setInterval(sendBang, d);
-  document.getElementById('output').innerHTML = "sending bang every " + d + " milliseconds";
+  document.getElementById('output').innerHTML = 
+    'sending bang every ' + d + ' milliseconds';
 }
 
+/**
+ * Send a metronome tick via Spacebrew
+ */
 function sendBang(){
   sb.send('bang', 'boolean', 'true');
-};
+}
