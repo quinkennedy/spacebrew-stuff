d66f73051d8ed6eab31db4dcfb8b332e3314c839
Quin Kennedy <quinkennedy@gmail.com>
2016-11-15 12:57:32 -0800

    linted web-filter

--- web-filter/js/main.js
+++ web-filter/js/main.js
@@ -1,31 +1,37 @@
+/* global Spacebrew */
+
 $(document).ready( function() {
-	setup();
+  setup();
 });
 
 var sb;
-var setup = function(){
+
+/**
+ * initialize the Spacebrew Client and connect to the Spacebrew server
+ */
+function setup(){
   sb = new Spacebrew.Client(undefined,
-                            "filter " + Math.floor(Math.random() * 1000),
-                            "This only passes true values",
+                            'filter ' + Math.floor(Math.random() * 1000),
+                            'This only passes true values',
                             {debug: true});
   window.document.title = sb._name;
-  sb.addPublish("true", "boolean", "true");
-  sb.addSubscribe("value", "boolean");
+  sb.addPublish('true', 'boolean', 'true');
+  sb.addSubscribe('value', 'boolean');
   sb.onBooleanMessage = function onBoolean(name, value){
     if (value){
-      sb.send("true", "boolean", "true");
+      sb.send('true', 'boolean', 'true');
     }
   };
   sb.onOpen = function onOpen(){
-    console.log("WebSockets connection opened");
-    document.getElementById("output").innerHTML = 
-      "Send a boolean to be filtered.";
+    console.log('WebSockets connection opened');
+    document.getElementById('output').innerHTML = 
+      'Send a boolean to be filtered.';
   };
   sb.onClose = function onClose(){
-    console.log("WebSockets connection closed");
-    document.getElementById("output").innerHTML = 
-      "no longer connected to spacebrew :(";
+    console.log('WebSockets connection closed');
+    document.getElementById('output').innerHTML = 
+      'no longer connected to spacebrew :(';
   };
 
   sb.connect();
-};
+}
