a8a4989a1100c840e9a9d11f7bd18f787ec6c06d
Quin Kennedy <quinkennedy@gmail.com>
2016-11-15 13:36:06 -0800

    linted web-send-url

--- web-send-url/js/main.js
+++ web-send-url/js/main.js
@@ -1,24 +1,31 @@
+/* global Spacebrew */
+
 $(document).ready( function() {
-	setup();
+  setup();
 });
 
 var sb;
+
+/**
+ * Configure this Spacebrew Client and connect to the Spacebrew server
+ */
 function setupSB(){
   sb = new Spacebrew.Client(undefined,
-                            "send-url " + Math.floor(Math.random() * 1000),
+                            'send-url ' + Math.floor(Math.random() * 1000),
                             'A simple image viewer',
                             {debug: true});
   window.document.title = sb._name;
   sb.addPublish('image', 
                 'url', 
-                'http://thissongissick.com/blog/wp-content/uploads/2011/05/Hall-and-Oates-Dj-Kue-Remix.jpg');
+                'http://thissongissick.com/blog/wp-content/uploads' +
+                  '/2011/05/Hall-and-Oates-Dj-Kue-Remix.jpg');
   sb.addPublish('cover', 'boolean', 'true');
 
   sb.onOpen = function onOpen(){
-    console.log("WebSockets connection opened");
+    console.log('WebSockets connection opened');
   };
   sb.onClose = function onClose(){
-    console.log("WebSockets connection closed");
+    console.log('WebSockets connection closed');
   };
   
   sb.connect();
@@ -27,25 +34,34 @@ function setupSB(){
 
 //-------------------------------------------------------
 var ddl, txt, chk;
+/**
+ * connect UI events
+ */
 function setup (){
-  ddl = document.getElementById("ddlImg");
-  txt = document.getElementById("txtImg");
-  document.getElementById("btnImg").addEventListener('click',function(e){
+  ddl = document.getElementById('ddlImg');
+  txt = document.getElementById('txtImg');
+  document.getElementById('btnImg').addEventListener('click',function(e){
     if (e){e.preventDefault();} sendImg();});
   ddl.onchange = function(){
     txt.value = ddl.options[ddl.selectedIndex].value;
     sendImg();
-  }
+  };
   txt.onchange = sendImg;
-  chk = document.getElementById("chkCover");
+  chk = document.getElementById('chkCover');
   chk.addEventListener('click', sendCover);
   setupSB();
 }
 
+/**
+ * Send a message via the 'cover' route based on the 'cover' checkbox
+ */
 function sendCover(){
   sb.send('cover', 'boolean', chk.checked.toString());
-};
+}
 
+/**
+ * Send the URL of the currently selected image
+ */
 function sendImg(){
   sb.send('image', 'url', txt.value);
-};
+}
